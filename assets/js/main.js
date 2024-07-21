8b2c1127154103e87b97c356238fd570143a0ba6
M. Budak <mbudak0079@gmail.com>
2024-07-21 16:51:33 +0300

    images-updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,51 +8,51 @@
 
 
 (function() {
-  document.addEventListener("DOMContentLoaded", function() {
-    let SHEET_ID = '1HHlDEC0XFvWjIrkWeBsEee4NsRR469msmezS69htQwQ';
-    let SHEET_TITLE = 'Database';
-    let SHEET_RANGE = 'A1:D29';
-    let FULL_URL = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?sheet=${SHEET_TITLE}&range=${SHEET_RANGE}`;
-  
-    fetch(FULL_URL)
-    .then(res => res.text())
-    .then(rep => {
-        console.log(rep); // Yanıtı kontrol etmek için
-  
-        // JSON verisini parse et
-        let jsonData = rep.substr(47).slice(0, -2);
-        let data = JSON.parse(jsonData);
-        console.log(data); // JSON verisini kontrol etmek için
-  
-        // Item listesi
-        let items = ['alfredo_small', 'alfredo_big', 'pesto', 'tavukmantar', 'deniz', 'patlican', 'bolognese', 'renklibiber', 'arrabiata', 'napoletana', 'danafume', 'portofino', 'mozzarella', 'kurudomates', 'parmesan', 'truf', 'cheddar', 'cola', 'icetea', 'gazoz', 'meyvesuyu', 'soda', 'ayran', 'su', 'turkkahvesi', 'cay', 'tatli'];
-  
-        for (let i = 0; i < items.length; i++) {
-            let item = items[i];
-            let row = i; // Satır indeksini belirle
-  
-            // Menü adı
-            let nameElement = document.getElementById(item + '_name');
-            if (nameElement) {
-                nameElement.innerHTML = data.table.rows[row].c[0] && data.table.rows[row].c[0].v ? data.table.rows[row].c[0].v : 'Yok';
-            }
-  
-            // Menü içeriği
-            let descriptionElement = document.getElementById(item + '_description');
-            if (descriptionElement) {
-                descriptionElement.innerHTML = data.table.rows[row].c[1] && data.table.rows[row].c[1].v ? data.table.rows[row].c[1].v : 'Açıklama mevcut değil';
-            }
-  
-            // Menü fiyatı
-            let priceElement = document.getElementById(item + '_price');
-            if (priceElement) {
-                priceElement.innerHTML = (data.table.rows[row].c[3] && data.table.rows[row].c[3].v ? data.table.rows[row].c[3].v : 'Fiyat bilinmiyor') + " TL";
-            }
-        }
-    })
-    .catch(error => console.error('Error fetching data:', error));
-  });
-  
+document.addEventListener("DOMContentLoaded", function() {
+  let SHEET_ID = '1HHlDEC0XFvWjIrkWeBsEee4NsRR469msmezS69htQwQ';
+  let SHEET_TITLE = 'Database';
+  let SHEET_RANGE = 'A1:D29';
+  let FULL_URL = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?sheet=${SHEET_TITLE}&range=${SHEET_RANGE}`;
+
+  fetch(FULL_URL)
+  .then(res => res.text())
+  .then(rep => {
+      console.log(rep); // Yanıtı kontrol etmek için
+
+      // JSON verisini parse et
+      let jsonData = rep.substr(47).slice(0, -2);
+      let data = JSON.parse(jsonData);
+      console.log(data); // JSON verisini kontrol etmek için
+
+      // Item listesi
+      let items = ['alfredo_small', 'alfredo_big', 'pesto', 'tavukmantar', 'deniz', 'patlican', 'bolognese', 'renklibiber', 'arrabiata', 'napoletana', 'danafume', 'portofino', 'mozzarella', 'kurudomates', 'parmesan', 'truf', 'cheddar', 'cola', 'icetea', 'gazoz', 'meyvesuyu', 'soda', 'ayran', 'su', 'turkkahvesi', 'cay', 'tatli'];
+
+      for (let i = 0; i < items.length; i++) {
+          let item = items[i];
+          let row = i; // Satır indeksini belirle
+
+          // Menü adı
+          let nameElement = document.getElementById(item + '_name');
+          if (nameElement) {
+              nameElement.innerHTML = data.table.rows[row].c[1] && data.table.rows[row].c[1].v ? data.table.rows[row].c[1].v : '';
+          }
+
+          // Menü içeriği
+          let descriptionElement = document.getElementById(item + '_description');
+          if (descriptionElement) {
+              descriptionElement.innerHTML = data.table.rows[row].c[2] && data.table.rows[row].c[2].v ? data.table.rows[row].c[2].v : '';
+          }
+
+          // Menü fiyatı
+          let priceElement = document.getElementById(item + '_price');
+          if (priceElement) {
+              priceElement.innerHTML = (data.table.rows[row].c[3] && data.table.rows[row].c[3].v ? data.table.rows[row].c[3].v : '') + " TL";
+          }
+      }
+  })
+  .catch(error => console.error('Error fetching data:', error));
+});
+
 
 
 
