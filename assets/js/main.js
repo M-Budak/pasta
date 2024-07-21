435144610a85bd57312e33ddefc97469a02daead
M. Budak <mbudak0079@gmail.com>
2024-07-21 15:44:44 +0300

    js-updatedV2

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,47 +7,47 @@
 
 
 
-(function() 
+(function() {
+  document.addEventListener("DOMContentLoaded", function() {
+      let SHEET_ID = '1HHlDEC0XFvWjIrkWeBsEee4NsRR469msmezS69htQwQ';
+      let SHEET_TITLE = 'Database';
+      let SHEET_RANGE = 'A1:D29';
+      let FULL_URL = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?sheet=${SHEET_TITLE}&range=${SHEET_RANGE}`;
 
-{
-  let SHEET_ID = '1HHlDEC0XFvWjIrkWeBsEee4NsRR469msmezS69htQwQ';
-  let SHEET_TITLE = 'Database';
-  let SHEET_RANGE = 'A1:D29';
-  let FULL_URL = ('https://docs.google.com/spreadsheets/d/' + SHEET_ID + '/gviz/tq?sheet=' + SHEET_TITLE + '&range=' + SHEET_RANGE);
-  
-  fetch(FULL_URL)
-  .then(res => res.text())
-  .then(rep => {
-      let data = JSON.parse(rep.substr(47).slice(0,-2));
-      console.log(rep);
-  
-      document.addEventListener("DOMContentLoaded", function() {
-          const items = ['alfredo_small', 'alfredo_big', 'pesto', 'tavukmantar', 'deniz', 'patlican', 'bolognese', 'renklibiber', 'arrabiata', 'napoletana', 'danafume', 'portofino', 'mozzarella', 'kurudomates', 'parmesan', 'truf', 'cheddar', 'cola', 'icetea', 'gazoz', 'meyvesuyu', 'soda', 'ayran', 'su', 'turkkahvesi', 'cay', 'tatli'];
-  
-          for (let i = 0; i < items.length; i++) {
-              let item = items[i];
-  
-              // Menü adı
-              let nameElement = document.getElementById(item + '_name');
-              if (nameElement) {
-                  nameElement.innerHTML = data.table.rows[i].c[0].v;
-              }
-  
-              // Menü içeriği
-              let descriptionElement = document.getElementById(item + '_description');
-              if (descriptionElement) {
-                  descriptionElement.innerHTML = data.table.rows[i].c[1].v;
-              }
-  
-              // Menü fiyatı
-              let priceElement = document.getElementById(item + '_price');
-              if (priceElement) {
-                  priceElement.innerHTML = data.table.rows[i].c[3].v + " TL";
-              }
+      fetch(FULL_URL)
+      .then(res => res.text())
+      .then(rep => {
+          console.log(rep); // Yanıtı kontrol etmek için
+
+          // JSON verisini parse et
+          let jsonData = rep.substr(47).slice(0, -2);
+          let data = JSON.parse(jsonData);
+          console.log(data); // JSON verisini kontrol etmek için
+
+          // Sadece alfredo_small öğesini işlemek için
+          let item = 'alfredo_small';
+
+          // Menü adı
+          let nameElement = document.getElementById(item + '_name');
+          if (nameElement) {
+              nameElement.innerHTML = data.table.rows[0].c[1].v;
           }
-      });
+
+          // Menü içeriği
+          let descriptionElement = document.getElementById(item + '_description');
+          if (descriptionElement) {
+              descriptionElement.innerHTML = data.table.rows[0].c[2].v;
+          }
+
+          // Menü fiyatı
+          let priceElement = document.getElementById(item + '_price');
+          if (priceElement) {
+              priceElement.innerHTML = data.table.rows[0].c[3].v + " TL";
+          }
+      })
+      .catch(error => console.error('Error fetching data:', error));
   });
-  
+
 
     // for(let i = 0; i<length;i++){
     //     let NewMenu = document.createElement('div');
---
+++ assets/js/menu.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let SHEET_ID = '1HHlDEC0XFvWjIrkWeBsEee4NsRR469msmezS69htQwQ';
+    let SHEET_TITLE = 'Database';
+    let SHEET_RANGE = 'A1:D29';
+    let FULL_URL = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?sheet=${SHEET_TITLE}&range=${SHEET_RANGE}`;
+
+    fetch(FULL_URL)
+    .then(res => res.text())
+    .then(rep => {
+        console.log(rep); // Yanıtı kontrol etmek için
+
+        // JSON verisini parse et
+        let jsonData = rep.substr(47).slice(0, -2);
+        let data = JSON.parse(jsonData);
+        console.log(data); // JSON verisini kontrol etmek için
+
+        // Sadece alfredo_small öğesini işlemek için
+        let item = 'alfredo_small';
+
+        // Menü adı
+        let nameElement = document.getElementById(item + '_name');
+        if (nameElement) {
+            nameElement.innerHTML = data.table.rows[0].c[1].v;
+        }
+
+        // Menü içeriği
+        let descriptionElement = document.getElementById(item + '_description');
+        if (descriptionElement) {
+            descriptionElement.innerHTML = data.table.rows[0].c[2].v;
+        }
+
+        // Menü fiyatı
+        let priceElement = document.getElementById(item + '_price');
+        if (priceElement) {
+            priceElement.innerHTML = data.table.rows[0].c[3].v + " TL";
+        }
+    })
+    .catch(error => console.error('Error fetching data:', error));
+});
